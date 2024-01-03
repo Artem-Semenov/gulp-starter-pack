47ea9c200954c2c665cf84ba095df3d5855d1d26
Artem <artem.semenov.101@gmail.com>
2024-01-03 20:26:58 +0200

    upd

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,4 +1,30 @@
 const config = {
+  mode: "development",
+  devtool: "source-map",
+  entry: {
+    index: "./src/js/index.js",
+    // about: "./src/js/about.js",
+    // contacts: './src/js/contacts.js',
+  },
+  output: {
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  optimization: {
+    minimize: false,
+  },
+};
+
+module.exports = config;
+
+/* const config = {
   mode: "production",
   devtool: "source-map",
   entry: {
@@ -23,3 +49,4 @@ const config = {
 };
 
 module.exports = config;
+ */
